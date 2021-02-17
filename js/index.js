7cbeb1f3642d95a4855efa6f85c732f63681cc0e
Lia <liasav19@ukr.net>
2021-02-17 22:49:04 +0200

    modal

---
+++ js/index.js
@@ -0,0 +1,7 @@
+const modal = $.modal({
+  title: "поздравляем, ты выиграл:",
+  closable: false,
+  content: `
+    <div class="modal-amount">500&#36;</div>
+    <div class="modal-info">Осталось неиспользованных вращений : 1</div>
+  `});
--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
 window.addEventListener("DOMContentLoaded", () => {
-  var btn = document.querySelector(".btn"),
-    wheel = document.querySelector(".wheel_inside");
+  const btn = document.querySelector(".btn"),
+    wheel = document.querySelector(".wheel_inside"),
+    btnModal = document.querySelector(".modal-btn");
 
-  btn.onclick = function () {
+  btn.addEventListener("click", function () {
     wheel.classList.toggle("animate");
-  };
+  });
+  btn.addEventListener("click", function () {
+    modal.open();
+  });
+  btnModal.addEventListener("click", function () {
+    modal.close();
+    wheel.classList.toggle("animateSecond");
+  });
 });
